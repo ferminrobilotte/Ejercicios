fd79902f5b3b5262a8380574f736572a1e0e761f
BAS-BBLA-N1-D15\Punto Digital <ferrobilotte@gmail.com>
2017-09-12 20:44:10 -0300

    no message

--- Ejercicio 1 validar/Ejercicio 2/Ej2javascript.js
+++
@@ -1,8 +0,0 @@
-function confirmarpagina(){
-	var r = confirm("Usted quiere abandonar la pagina?");
-	if (r==true){
-		location.assing("https://www.google.com.ar");
-	} else{
-		return false;
-	}
-}
