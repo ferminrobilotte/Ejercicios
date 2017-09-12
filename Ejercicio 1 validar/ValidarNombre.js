b7b45a19ee61866037bbb7b58d461abfd4b0cc59
BAS-BBLA-N1-D15\Punto Digital <ferrobilotte@gmail.com>
2017-09-12 19:51:17 -0300

    Ejercico hecho

--- Ejercicio 1 validar/ValidarNombre.js
+++ Ejercicio 1 validar/ValidarNombre.js
@@ -8,20 +8,23 @@ function validar() {
 	if(edad.value<18){
 		alert("Edad menor a 18")
 	};
-	mostrarocultar();
+	
 }
 function mostrar(trabajo){
-	document.getElementById("trabajo").style.display="block";
+	trabajo.style.display="block";
 }
 
 function ocultar(trabajo){
-	document.getElementById("trabajo").style.display="none";
+	trabajo.style.display="none";
 }
 
 function mostrarocultar(){
-	var b = document.getElementById("hombre");
-	if(b.value=="1"){
-		Mostar(document.getElementById("trabajohombre"));
+	var s = document.getElementById("sexo")
+	if (s.value=="1"){
+		mostrar(document.getElementById("trabajohombre"));
+		ocultar(document.getElementById("trabajomujer"));
+	} else{
+		mostrar(document.getElementById("trabajomujer"));
+		ocultar(document.getElementById("trabajohombre"));
 	}
-
 }
